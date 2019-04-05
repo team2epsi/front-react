a3867c85adbf61d790c46fc6296160038cb0fd55
H <youthef@hotmail.fr>
2019-04-05 11:53:09 +0200

    Test connexion API

--- src/App/index.js
+++ src/App/index.js
@@ -3,20 +3,38 @@ import Header from "../Header";
 import ExpeditionList from "../ExpeditionList";
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
+  constructor() {
+    super();
 
-  //   this.state = {
-  //     products: [],
-  //     basketItems: []
-  //   };
-  // }
+    this.state = {
+      products: [],
+      basketItems: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://sdiscount-api.herokuapp.com/products")
+      .then(response => response.json())
+      .then(data => {
+        const products = data.map(product => ({
+          // description: product.description,
+          // imageUrl: product.imageUrl,
+          // price: product.price,
+          // reference: product.reference,
+          title: product.title
+        }));
+
+        this.setState({
+          products
+        });
+      });
+  }
 
   render() {
     return (
       <div>
         <Header title="Loclandes" />
-        <ExpeditionList />
+        <ExpeditionList products={this.state.products} />
       </div>
     );
   }
