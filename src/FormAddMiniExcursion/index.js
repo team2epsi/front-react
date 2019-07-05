732b2156542e8fb86d90284ba37124a082468b03
H <youthef@hotmail.fr>
2019-07-05 16:52:33 +0200

    Modification du bootstrap

--- src/FormAddMiniExcursion/index.js
+++ src/FormAddMiniExcursion/index.js
@@ -1,65 +1,5 @@
 import React from "react";
 import "./styles.css";
-import { Button, Modal } from "react-bootstrap";
-
-// const FormAddMiniExcursion = props => (
-//   <React.Fragment>
-//     <div className="DashboardTool">
-//       {/* <!-- Button trigger modal --> */}
-//       <button
-//         type="button"
-//         className="btn btn-success"
-//         data-toggle="modal"
-//         data-target="#exampleModalLong"
-//       >
-//         + Ajouter une mini-excursion
-//       </button>
-//     </div>
-
-//     {/* <!-- Modal --> */}
-//     <div
-//       className="modal fade"
-//       id="exampleModalLong"
-//       tabIndex="-1"
-//       role="dialog"
-//       aria-labelledby="exampleModalLongTitle"
-//       aria-hidden="true"
-//     >
-//       <div className="modal-dialog" role="document">
-//         <div className="modal-content">
-//           <div className="modal-header">
-//             <h5 className="modal-title" id="exampleModalLongTitle">
-//               Modal title
-//             </h5>
-//             <button
-//               type="button"
-//               className="close"
-//               data-dismiss="modal"
-//               aria-label="Close"
-//             >
-//               <span aria-hidden="true">&times;</span>
-//             </button>
-//           </div>
-//           <div className="modal-body">...</div>
-//           <div className="modal-footer">
-//             <button
-//               type="button"
-//               className="btn btn-secondary"
-//               data-dismiss="modal"
-//             >
-//               Close
-//             </button>
-//             <button type="button" className="btn btn-primary">
-//               Save changes
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </React.Fragment>
-// );
-
-// export default FormAddMiniExcursion;
 
 class FormAddMiniExcursion extends React.Component {
   constructor(props, context) {
@@ -74,10 +14,12 @@ class FormAddMiniExcursion extends React.Component {
   }
 
   handleClose() {
+    console.log("click on close");
     this.setState({ show: false });
   }
 
   handleShow() {
+    console.log("click on save");
     this.setState({ show: true });
   }
 
@@ -85,36 +27,110 @@ class FormAddMiniExcursion extends React.Component {
     return (
       <React.Fragment>
         <div className="DashboardTool">
-          <Button
+          <button
+            type="button"
             className="btn btn-success"
-            variant="primary"
-            onClick={this.handleShow}
+            data-toggle="modal"
+            data-target="#exampleModalLong"
           >
             + Ajouter une mini-excursion
-          </Button>
+          </button>
+        </div>
 
-          <Modal show={this.state.show} onHide={this.handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              Woohoo, you're reading this text in a modal!
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={this.handleClose}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
-          </Modal>
+        <div
+          className="modal fade"
+          id="exampleModalLong"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLongTitle"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle">
+                  Formulaire Mini-excursion
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="form-group">
+                    <label for="exampleInputText">
+                      Titre de la mini-excursion
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Titre Mini-Excursion"
+                      aria-label="MiniExcursion"
+                      aria-describedby="textHelp"
+                    />
+                    <small id="textHelp" className="form-text text-muted">
+                      Titre : Mettre une information en relation avec le champ.
+                    </small>
+                  </div>
+
+                  <div className="form-group">
+                    <label for="exampleInputText">
+                      Nombre de places de la mini-excursion (Max)
+                    </label>
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Nombre de places Mini-Excursion"
+                      aria-label="NombrePlaces"
+                      aria-describedby="numberHelp"
+                      min="1"
+                      max="50"
+                    />
+                    <small id="numberHelp" className="form-text text-muted">
+                      Places : Mettre une information en relation avec le champ.
+                    </small>
+                  </div>
+                  <div className="form-group form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="exampleCheck1"
+                    />
+                    <label className="form-check-label" for="exampleCheck1">
+                      Publication
+                    </label>
+                  </div>
+                </form>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                    onClick={this.handleClose}
+                  >
+                    Fermer
+                  </button>
+                  {/* Type = button ? */}
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onClick={this.handleShow}
+                  >
+                    Créer la mini-excursion
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
   }
 }
-
-//   render(<Example />);
 
 export default FormAddMiniExcursion;
